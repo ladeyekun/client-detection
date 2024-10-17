5d415fdff2d7d71275f5c782fdc5c4b52d727d4f
Lateef Adeyekun <lateef.adeyekun@gmail.com>
2024-10-17 12:07:19 -0500

    battery update

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,26 +16,92 @@ const windowHeight = select('.window-height');
 const windowOrientation = select('.window-orientation');
 const batteryLevel = select('.battery-level');
 const batteryStatus = select('.battery-status');
+const onlineStatus = select('.box-status h2');
+
+listen('load', window, () => {
+    loadWindow();
+    loadSystem();
+    updateNetworkStatus();
+    loadBatteryInfo();
+});
+
+listen('resize', window, () => {
+    loadWindow();
+});
+
+listen('orientationchange', window, () => {
+    loadWindow();
+});
+
+listen('online', window, () => {
+    updateNetworkStatus();
+});
+
+listen('offline', window, () => {
+    updateNetworkStatus();
+});
 
 function loadWindow() {
-    windowWidth.innerText = `${window.innerWidth}px`;
-    windowHeight.innerText = `${window.innerHeight}px`;
-    windowOrientation = window.orientation;
+    windowWidth.innerText = `${winWidth()}px`;
+    windowHeight.innerText = `${winHeight()}px`;
+    windowOrientation.innerText = getScreenOrientation();
 }
 
-function getScreenOrientation(type) {
-    switch(type) {
-        case 'landscape-primary':
-        case 'landscape-secondary':
-            return 'landscape';
-            break;
+function loadSystem() {
+    sysLang.innerText = getSysLang();
+    //sysOS.innerText = getSysOS();
+}
 
-        case 'portrait-primary':
-        case 'portrait-secondary':
-            return 'landscape';
-            break;
-        
+function updateNetworkStatus() {
+    onlineStatus.innerText = isNetworkOnline ? 'Online' : 'Offline';
+}
+
+const winWidth = () => window.innerWidth;
+const winHeight = () => window.innerHeight;
+
+const getSysLang = () => navigator.language;
+
+function getScreenOrientation() {
+    let type = window.screen.orientation.type;
+    return type.startsWith('landscape') ? 'landscape' : 'portrait';
+}
+
+function loadBatteryInfo() {
+    if ('getBattery' in navigator) {
+        navigator.getBattery().then(battery => {
+            updateBatteryInfo(battery);
+
+            listen('levelchange', battery, () => {
+                updateBatteryInfo(battery);
+            });
+            
+            listen('chargingchange', battery, () => {
+                updateBatteryInfo(battery);
+            });
+        });
+    } else {
+        batteryLevel.innerText = 'Not available';
+        batteryStatus.innerText = 'Not available';
     }
 }
-console.log(window);
 
+function updateBatteryInfo(battery) {
+   
+    batteryLevel.innerText = `${Math.round(battery.level * 100)}%`;
+
+    batteryStatus.innerText = battery.charging ? 'Plugged in' : 'Idle';
+}
+
+
+
+function getSysOS() {
+    let userAgent = navigator.userAgent;
+    console.log(userAgent);
+    const match = '/([A-Z][a-z]+)\/(\d+\.\d+)/';
+    const matches = userAgent.search(match);
+    return matches;
+}
+
+function isNetworkOnline() {
+    return navigator.onLine ? true : false;
+}
